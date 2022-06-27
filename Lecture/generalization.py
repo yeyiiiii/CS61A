150689c1010522db7b8f4a23ce59c18a774f64ad
BadPlusTea <30396996+yeyiiiii@users.noreply.github.com>
2022-06-27 14:18:20 +0800

    Update generalization.py

--- Lecture/generalization.py
+++ Lecture/generalization.py
@@ -15,3 +15,39 @@ def area_circle(r):
 def area_hexagon(r):
   return area(r, 3 * sqrt(3) / 2)
 
+#part2
+
+def identity(k):
+  return k
+
+def cube(k):
+  return pow(k, 3)
+
+def summation(n, term):
+  """Sum the first N terms of a sequence.
+  
+  >>> summation(5, cube)
+  225
+  """
+  total, k = 0, 1
+  while k <= n:
+    total, k = total + term(k), k + 1
+   return total
+
+def sum_naturals(n):
+  """Sum the first N natural numbers.
+  
+  >>> sum_naturals(5)
+  15
+  """
+  return summation(n, identity)
+
+def sum_cubes(n):
+  """Sum the first N cubes of natural numbers.
+  
+  >>> sum_cubes(5)
+  225
+  """
+  return summation(n, cube)
+
+#python3 -m doctest -v generalization.py
